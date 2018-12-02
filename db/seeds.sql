1154ec034f8e0577c4934c8325bcea116f2234a2
Ed Skillion <edskillion@outlook.com>
2018-12-01 21:52:14 -0800

    burger assignment

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoked Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey Glazed Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Bacon Swiss Burger', false);
